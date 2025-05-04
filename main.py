a19a0938b4759677de769b4febfa6c2960f25ac6
goldensyrup-png <thegoldenachievers@gmail.com>
2025-05-04 03:22:40 +0100

    ghrhb

--- main.py
+++ main.py
@@ -1,41 +1,101 @@
 from flask import Flask, redirect, url_for, request, render_template
 import pandas as pd
-import os
+from datetime import date
 
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-    user_ip = request.remote_addr
-    return render_template('User.html', user_ip=user_ip, username = user, DateLast = date, steps = steps, percentage = Percentage)
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
 
-@app.route('/fail')
+@app.route('/NewData', methods=['GET', 'POST'])
+def NewData():
+    global steps, Globaldate, Percentage
+    if request.method == 'POST':
+        Steps = int(request.form['sp'])
+        LocalUsername = user
+        LocalPassword = password
+
+        df = pd.read_csv('data.csv')
+
+        user_row = df[(df['username'] == LocalUsername) & (df['password'] == LocalPassword)]
+
+        row_index = user_row.index[0]
+        CurrentSteps = df.at[row_index, 'total']
+        NewSteps = int(CurrentSteps) + Steps
+
+        df.at[row_index, 'total'] = NewSteps
+        today = date.today()
+        formatted = today.strftime('%d-%m-%Y')
+        df.at[row_index, 'date'] = formatted
+
+        df.to_csv('data.csv', index=False)
+
+        steps = NewSteps
+        Globaldate = formatted
+        Percentage = "{:.2f}".format((df["total"].iloc[0] / 1091450) * 100)
+
+        return redirect(url_for('success', Encryption=Encryption))
+    else:
+        user_ip = request.remote_addr
+        return render_template('NewData.html', user_ip=user_ip, username = user)
+
+@app.route('/success/<Encryption>', methods=['GET', 'POST'])
+def success(Encryption):
+    global Percentage
+    if request.method == 'POST':
+        return redirect(url_for('NewData'))
+    else:
+        user_ip = request.remote_addr
+        if int("{:.0f}".format(float(Percentage))) > 100:
+            Percentage = 100
+        return render_template('User.html', user_ip=user_ip, username = user, DateLast = Globaldate, steps = steps, percentage = Percentage, Encryption = Encryption)
+
+@app.route('/NewLogin', methods=['GET', 'POST'])
+def NewLogin():
+    if request.method == 'POST':
+        return "You pressed a button"
+    else:
+        return render_template('NewLogin.html')
+
+@app.route('/fail', methods=['GET', 'POST'])
 def fail():
-    user_ip = request.remote_addr
-    return render_template('fail.html', user_ip=user_ip)
+    if request.method == 'POST':
+        return render_template('login.html')
+    else:
+        user_ip = request.remote_addr
+        return render_template('fail.html', user_ip=user_ip)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        global user, steps, date, Percentage
-        user = request.form['nm']
-        password = request.form['pw']
-        Data = pd.read_csv("data.csv")
-        Data = Data.loc[Data['username'] == user]
-        Data = Data.loc[Data['password'] == password]
-        if not Data.empty:
-            # More Data #
-            Percentage = "{:.2f}".format((Data["total"].iloc[0] / 1091450) * 100)
-            date = Data["date"].iloc[0]
-            steps = Data["total"].iloc[0]
-            
-            return redirect(url_for('success', name=user))
-        else:
-            message = (f'{user} failed to access their account with password {password}!')
+        # Submit button pressed #
+        global user, steps, Globaldate, Percentage, Encryption, password
+        button_clicked = request.form['button']
+        if button_clicked == "Submit":
+            user = request.form['nm']
+            password = request.form['pw']
+            Data = pd.read_csv("data.csv")
+            Data = Data.loc[Data['username'] == user]
+            Data = Data.loc[Data['password'] == password]
+            if not Data.empty:  # Success #
+                
+                # More Data #
+                Percentage = "{:.2f}".format((Data["total"].iloc[0] / 1091450) * 100)
+                Globaldate = Data["date"].iloc[0]
+                steps = Data["total"].iloc[0]
+                Encryption = Data['encryption'].iloc[0]
+                
+                return redirect(url_for('success', Encryption=Encryption))
+            else:  # Failed #
+                message = (f'{user} failed to access their account with password {password}!')
 
-            with open('failure.txt', 'a') as file:
-                file.write(f"\n {message}")
-            return redirect(url_for('fail'))
+                with open('failure.txt', 'a') as file:
+                    file.write(f"\n {message}")
+                return redirect(url_for('fail'))
+        else:
+            # New Account Button Pressed #
+            return redirect(url_for('NewLogin'))
     else:
         return render_template('login.html')
 
